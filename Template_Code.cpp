bd43cd368c9e44e19e60fe397ad6682d6d0b6211
Lighteldin <noureldina2005@gmail.com>
2026-09-22 17:11:00 +0300

    Implement flipHorizontal, adjustBrightness, applyBlur

--- Template_Code.cpp
+++ Template_Code.cpp
@@ -180,11 +180,15 @@ Image flipHorizontal(const Image& input) {
     int width = input.getWidth();
     int channels = input.getChannels();
     Image output(width, height, channels);
-    
-    // TODO: Implement this function
-    // For each pixel and each channel:
-    //   output(y, width-1-x, c) = input(y, x, c)
-    
+
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            for (int c = 0; c < channels; c++) {
+                output(y, width - 1 - x, c) = input(y, x, c);
+            }
+        }
+    }
+
     return output;
 }
 
@@ -230,12 +234,16 @@ Image adjustBrightness(const Image& input, int value) {
     int width = input.getWidth();
     int channels = input.getChannels();
     Image output(width, height, channels);
-    
-    // TODO: Implement this function
-    // For each pixel and each channel:
-    //   new_value = input(y, x, c) + value
-    //   output(y, x, c) = max(0, min(255, new_value))
-    
+
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            for (int c = 0; c < channels; c++) {
+                int new_val = input(y, x, c) + value;
+                output(y, x, c) = max(0, min(255, new_val));
+            }
+        }
+    }
+
     return output;
 }
 
@@ -284,15 +292,22 @@ Image applyBlur(const Image& input) {
     int height = input.getHeight();
     int width = input.getWidth();
     int channels = input.getChannels();
-    Image output(width, height, channels);
-    
-    // TODO: Implement this function
-    // For each pixel (from y=1 to height-2, x=1 to width-2) and each channel:
-    //   sum = 0
-    //   For each neighbor (ky from -1 to 1, kx from -1 to 1):
-    //     sum += input(y+ky, x+kx, c)
-    //   output(y, x, c) = sum / 9
-    
+    Image output(width, height, channels); 
+
+    for (int y = 1; y <= height - 2; y++) {
+        for (int x = 1; x <= width - 2; x++) {
+            for (int c = 0; c < channels; c++) {
+                int sum = 0;
+                for (int ky = -1; ky <= 1; ky++) {
+                    for (int kx = -1; kx <= 1; kx++) {
+                        sum += input(y + ky, x + kx, c);
+                    }
+                }
+                output(y, x, c) = sum / 9; 
+            }
+        }
+    }
+
     return output;
 }
 
